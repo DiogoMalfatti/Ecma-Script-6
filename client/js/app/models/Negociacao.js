7d55235f3ddccd526127034a5f5c9b84a89d1041
DiogoMalfatti <diimalfatti@gmail.com>
2020-01-07 08:09:43 -0300

    teste da extensao git para vsc "V"

--- client/js/app/models/Negociacao.js
+++ client/js/app/models/Negociacao.js
@@ -1,20 +1,30 @@
 class Negociacao {
-    constructor (data, quantidade, valor) {
-        this.data = data;
-        this.quantidade = quantidade;
-        this.valor = valor;
+    
+    constructor(data, quantidade, valor) {
+        
+        this._data = new Date(data.getTime());
+        this._quantidade = quantidade;
+        this._valor = valor;
+        Object.freeze(this);
     }
-    getVolume() {
-        return this.quantidade * this.valor;
+    
+    get volume() {
+        
+        return this._quantidade * this._valor;
     }
-    getData() {
-        return this.data;
+    
+    get data() {
+        
+        return new Date(this._data.getTime());
     }
-    getQuantidade() {
-        return this.quantidade;
+    
+    get quantidade() {
+        
+        return this._quantidade;
     }
-    getValor() {
-        return this.valor;
+    
+    get valor() {
+        
+        return this._valor;
     }
-
 }
